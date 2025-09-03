bc62c5bc83912b6bd4644028ad67b9c5f6fc4e9c
Diego González <diegonurr@hotmail.com>
2025-09-03 17:51:22 -0400

    chore: attempt to fix GitHub Pages deployment again 2

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 		fallback: '404.html'
 	}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? 'diegurra' :''
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
 		alias: {
       "@/*": "./src/lib/*",
