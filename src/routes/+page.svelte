3198a3840880a61343d2a76c10d40b71f20b3819
Diego González <diegonurr@hotmail.com>
2025-09-03 20:59:06 -0400

    feat: added Proyectos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script>
-  import {Button } from "$lib/components/ui/button/index.js";
   import * as Card from "$lib/components/ui/card/index.js";
+  import { buttonVariants } from "$lib/components/ui/button/index.js";
+  import * as Dialog from "$lib/components/ui/dialog/index.js";
+  import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
+
+  import SvelteMarkdown from "@humanspeak/svelte-markdown";
   import {Mail, Github, Linkedin} from '@lucide/svelte';
   const bio = {
     nombre: "Diego González Urra",
@@ -26,8 +30,54 @@
     contacto: [
       {tipo: "Email", val: "diegonurr@hotmail.com", link: "mailto:diegonurr@hotmail.com"},
       {tipo: "Github", val: "github.com/DiegoGUrra", link: "https://github.com/DiegoGUrra"},
-      {tipoo: "Linkedin", val:"diegonurr", link:"https://www.linkedin.com/in/diegonurr/"}
-    ]
+      {tipo: "Linkedin", val:"diegonurr", link:"https://www.linkedin.com/in/diegonurr/"}
+    ],
+    portafolio: [{nombre:"Hola",
+    resumen: "resumido",
+    habilidades: ["Kali Linux","Metasploit","Python"],
+    md:
+      `
+# Resumen de Incidente
+
+**Empresa:** CyberSafe Solutions (ficticia)  
+
+### Resumen del incidente
+CyberSafe Solutions detectó un acceso no autorizado a información confidencial a través de la cuenta de un usuario, quien reportó actividad sospechosa. El análisis inicial reveló que las credenciales del usuario fueron comprometidas mediante un ataque de phishing, permitiendo que la cuenta fuera utilizada para extraer datos hacia una dirección IP externa.
+
+### Análisis de la situación
+El incidente parece haberse originado a partir de un ataque de phishing dirigido, mediante el cual se logró instalar un malware tipo keylogger en el equipo del usuario. Este tipo de malware registra la información ingresada por teclado y la envía al atacante, permitiendo la captura de credenciales y otros datos sensibles.  
+
+Una vez obtenidas estas credenciales, los atacantes accedieron a sistemas internos sin restricciones adicionales, debido a que la cuenta no contaba con autenticación multifactor (MFA).  
+
+Posteriormente, se identificó tráfico de datos inusual hacia una IP externa, lo que sugiere una posible exfiltración de información confidencial. La combinación de phishing, malware y ausencia de MFA facilitó un acceso no autorizado y persistente a la infraestructura interna, aumentando el riesgo para los activos críticos de la organización.
+
+# Prueba de Metasploit
+
+## I. Generación de Payload
+Se creó un payload de malware utilizando \`msfvenom\` con \`reverse_tcp\`, apuntando a la máquina atacante y seleccionando un puerto no utilizado.
+
+![Generación del Payload](/images/Imagen1.png)
+## II. Detección del Malware
+Un escaneo en VirusTotal indicó que el archivo era detectado como malware por 59 motores antivirus.
+
+![Detección de Malware](/images/Imagen2.png)
+## III. Distribución del Archivo Malicioso
+El archivo se distribuyó mediante un servidor HTTP creado con Python.
+
+![Creación de servidor HTTP](/images/Imagen3.png)
+
+## IV. Ejecución y Conexión Inversa
+El payload establece un “shell inverso” hacia la máquina del atacante.
+![Ejecución del archivo malicioso](/images/Imagen4.png)
+![Conexión en Metasploit](/images/Imagen5.png)
+![Sesión en Metasploit](/images/Imagen6.png)
+
+
+## V. Actividad del Keylogger
+Con la sesión establecida, se puede observar la actividad del keylogger, capturando credenciales y otra información sensible.
+![Keylogger en Metasploit](/images/Imagen7.png)
+`},
+]
   }
 </script>
 
@@ -73,25 +123,36 @@
   </div>
   
 </section>
-
 <section class="bg-secondary p-6">
   <h2 class="text-foreground text-4xl font-bold p-6">Proyectos</h2>
   <div class="flex item-center justify-center">
-    {#each bio.experiencia as exp}
+    {#each bio.portafolio as port}
     <Card.Root class="w-160 shadow-lg">
     <Card.Header>
       <div class="flex justify-between items-center">
-        <Card.Title>{exp.empresa}</Card.Title>
-        <Card.Title>{exp.fechaInicio} - {exp.fechaTermino}</Card.Title>
+        <Card.Title>{port.nombre}</Card.Title>
       </div>
-      <Card.Description>{exp.cargo}</Card.Description>
+      <Card.Description>{port.resumen}</Card.Description>
+      <Card.Action>
+        <Dialog.Root>
+          <Dialog.Trigger class={buttonVariants({variant: "outline"})}>Ver Detalle</Dialog.Trigger>
+          <Dialog.Content class="w-full sm:max-w-[90vw] h-[90vh] flex flex-col p-0">
+              <ScrollArea class="h-full p-6 rounded-md border flex-1">
+              <article class="prose prose-invert max-w-none text-justify">
+                <SvelteMarkdown source={port.md}/>
+              </article>
+            </ScrollArea>
+            </Dialog.Content>
+        </Dialog.Root>
+      </Card.Action>
     </Card.Header>
     <Card.Content>
-      <ul class="list-disc pl-4 space-y-2">
-        {#each exp.desc as tarea}
-        <li class="text-justify">{tarea}</li>
+      <div class="flex flex-wrap gap-2">
+        {#each port.habilidades as tarea}
+        <span class="text-justify bg-chart-2 rounded-full text-sm font-medium px-3 py-1 hover:bg-destructive cursor-default">{tarea}</span>
         {/each}
-      </ul>
+      </div>
+        
     </Card.Content>
   </Card.Root>
     {/each}
@@ -130,21 +191,3 @@
   </a>
 </div>
 </section>
-
-
-
-
-<!-- <section class="bg-primary max-w-3xl mx-auto flex flex-wrap gap-4 mt-6">
-  <Card.Root class="w-screen bg-card text-card-foreground">
-  <Card.Header>
-    <Card.Title class="">{bio.nombre}</Card.Title>
-    <Card.Description>
-      <p>{bio.desc}</p>
-    </Card.Description>
-  </Card.Header>
-  <Card.Content>
-  </Card.Content>
-  <Card.Footer>
-  </Card.Footer>
-  </Card.Root>
-</section> -->
